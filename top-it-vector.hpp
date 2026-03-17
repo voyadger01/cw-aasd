c40a7e92e21ca47ae9cc9bd4568bed09908e9712
voyadger01 <voyadgerkx@gmail.com>
2026-03-17 13:26:10 +0300

    fail test

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -11,10 +11,30 @@ namespace topit
     size_t size_, capacity_;
   public:
     Vector< T >();
+    Vector< T >(const Vector< T >&) = delete;
+    Vector< T >& operator=(const Vector< T >&) = delete;
     ~Vector< T >();
+
     bool isEmpty() const noexcept;
-    
   };
 }
 
+template< class T >
+topit::Vector< T >::Vector():
+  data_(nullptr),
+  size_(0),
+  capacity_(0)
+{}
+
+template< class T >
+topit::Vector< T >::~Vector<T>()
+{
+  delete [] data_;
+}
+
+template< class T >
+bool topit::Vector< T >::isEmpty() const noexcept
+{
+  return false;
+}
 #endif
