abc625f451966a422f7b6374bf35c387d2fe6c64
voyadger01 <voyadgerkx@gmail.com>
2026-03-25 19:00:56 +0300

    from size_t to long long

--- top-it-iterator.hpp
+++ top-it-iterator.hpp
@@ -2,26 +2,28 @@
 #define TOP_IT_VECTOR_ITER_HPP
 #include <cstddef>
 namespace topit {
+  using signed_size_t = long long;
   template < class T > class Vector;
   template < class T > class LIter {
     friend class Vector< T >;
-    friend LIter operator+(size_t, const LIter &);
+    friend LIter operator+(signed_size_t, const LIter &);
     Vector< T > *vec_;
     size_t idx_;
   public:
     LIter();
     LIter(Vector< T > *p, size_t idx);
     T &operator*() const;
-    T &operator[](size_t n) const;
+    T *operator->() const;
+    T &operator[](signed_size_t n) const;
     LIter &operator++();
     LIter operator++(int);
     LIter &operator--();
     LIter operator--(int);
-    LIter &operator+=(size_t n);
-    LIter &operator-=(size_t n);
-    LIter operator+(size_t n) const;
-    LIter operator-(size_t n) const;
-    size_t operator-(const LIter &other) const;
+    LIter &operator+=(signed_size_t n);
+    LIter &operator-=(signed_size_t n);
+    LIter operator+(signed_size_t n) const;
+    LIter operator-(signed_size_t n) const;
+    signed_size_t operator-(const LIter &other) const;
     bool operator==(const LIter &other) const;
     bool operator!=(const LIter &other) const;
     bool operator<(const LIter &other) const;
@@ -31,7 +33,7 @@ namespace topit {
   };
   template < class T > class LCIter {
     friend class Vector< T >;
-    friend LCIter operator+(size_t, const LCIter &);
+    friend LCIter operator+(signed_size_t, const LCIter &);
     const Vector< T > *vec_;
     size_t idx_;
   public:
@@ -39,16 +41,17 @@ namespace topit {
     LCIter(const Vector< T > *p, size_t idx);
     LCIter(const LIter< T > &other);
     const T &operator*() const;
-    const T &operator[](size_t n) const;
+    const T *operator->() const;
+    const T &operator[](signed_size_t n) const;
     LCIter &operator++();
     LCIter operator++(int);
     LCIter &operator--();
     LCIter operator--(int);
-    LCIter &operator+=(size_t n);
-    LCIter &operator-=(size_t n);
-    LCIter operator+(size_t n) const;
-    LCIter operator-(size_t n) const;
-    size_t operator-(const LCIter &other) const;
+    LCIter &operator+=(signed_size_t n);
+    LCIter &operator-=(signed_size_t n);
+    LCIter operator+(signed_size_t n) const;
+    LCIter operator-(signed_size_t n) const;
+    signed_size_t operator-(const LCIter &other) const;
     bool operator==(const LCIter &other) const;
     bool operator!=(const LCIter &other) const;
     bool operator<(const LCIter &other) const;
@@ -56,7 +59,7 @@ namespace topit {
     bool operator<=(const LCIter &other) const;
     bool operator>=(const LCIter &other) const;
   };
-  template < class T > LIter< T > operator+(size_t, const LIter< T > &);
-  template < class T > LCIter< T > operator+(size_t, const LCIter< T > &);
+  template < class T > LIter< T > operator+(signed_size_t, const LIter< T > &);
+  template < class T > LCIter< T > operator+(signed_size_t, const LCIter< T > &);
 }
 #endif
